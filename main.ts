79b065bd15200a3b0882fe12490f9a3101950432
Albert Avery <albertavery@Alberts-MacBook-Pro.local>
2018-09-25 16:09:28 -0700

    cleaned up ts

--- main.ts
+++ main.ts
@@ -1,20 +1,14 @@
-
-
 class Greeter {
   constructor(private first_name: string, private last_name: string) {
   }
   showGreeting() {
     const banner = document.getElementById('banner');
-    banner.innerHTML = 'Hello ' + this.first_name+ ' ' + this.last_name + '.';
+    banner.innerHTML = 'Hello ' + this.first_name + ' ' + this.last_name + '.';
   }
-
   hideGreeting() {
     const banner = document.getElementById('banner');
-    banner.innerHTML = 'r1c2'; 
+    banner.innerHTML = 'r1c2';
   }
-
-
 }
 
 const greeter = new Greeter('Albert', 'Avery');
-
